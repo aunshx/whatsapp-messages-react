4518816b2820885dd005a4dc73f6a693f1ee727f
aunshx <aunsh.sb@gmail.com>
2022-03-19 08:27:24 +0530

    basic working functionality

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import { Button } from "@mui/material";
+import { Button, TextField, styled } from "@mui/material";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 import { makeStyles } from "@mui/styles";
@@ -15,14 +15,46 @@ const loginIconButtonStyle = makeStyles({
       backgroundColor: "transparent",
       color: "#968F73",
       border: "1px solid #968F73",
-    },
+    }
   },
 });
 
+const CssTextField = styled(TextField, {
+  shouldForwardProp: (props) => props !== "focusColor",
+})((p) => ({
+  // input label when focused
+  "& label.Mui-focused": {
+    color: "#7ed957",
+  },
+  // focused color for input with variant='outlined'
+  "& .MuiOutlinedInput-root": {
+    "&.Mui-focused fieldset": {
+      borderColor: "#7ed957",
+      fontSize: "0.9em",
+    },
+    borderRadius: '10px'
+  },
+}));
+
+const textFieldInputLabelStyle = {
+  fontSize: "0.9em",
+  alignSelf: "center",
+  justifySelf: "center",
+};
+
+const textFieldStyle = {
+  width: "230px",
+};
+
 const SendMessages = () => {
+  const CHARACTER_LIMIT = 100;
+
   const URL = "https://web.whatsapp.com/send";
 
-  const [input, setInput] = useState("");
+    const [numberEmptyError, setNumberEmptyError] = useState(false);
+    const [messageEmptyError, setMessageEmptyError] = useState(false);
+    const [notAnInteger, setNotAnInteger] = useState(false)
+
   const [formData, setFormData] = useState({
     mobileNumber: "",
     message: "",
@@ -38,34 +70,110 @@ const SendMessages = () => {
     });
   };
 
-  // number = number.replace(/[^\w\s]/gi, '').replace(/ /g, '')
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (mobileNumber.length < 1){
+        setNumberEmptyError(true)
+        setTimeout(() => setNumberEmptyError(false), 3000);
+    } else if (!isFinite(String(mobileNumber))){
+        setNotAnInteger(true)
+        setTimeout(() => setNotAnInteger(false), 3000)
+    } else if (message.length < 1) {
+        setMessageEmptyError(true)
+        setTimeout(() => setMessageEmptyError(false), 3000)
+    } else {
+        let number = mobileNumber.replace(/[^\w\s]/gi, "").replace(/ /g, "");
 
-  // let url = `${URL}?phone=${number}`
+        let url = `${URL}?phone=${number}`;
 
-  // if (message) {
-  //     url += `&text=${encodeURI(message)}&app_absent=0`
-  // }
+       url += `&text=${encodeURI(message)}&app_absent=0`;
+      window.open(url)
+    }
+  }
 
   return (
     <div className='communication'>
-      <div className='whatsapp-card'>
+      <div className='whatsapp-card app'>
         <div className='title flex_middle'>
           <div style={{ marginRight: "0.5em" }}>
             <WhatsAppIcon />
           </div>
           <div>Send Message</div>
         </div>
+        {numberEmptyError && (
+          <div className='errors'>Mobile number cannot be empty!</div>
+        )}
+        {notAnInteger && (
+          <div className='errors'>Mobile number should be an integer!</div>
+        )}
+        {messageEmptyError && (
+          <div className='errors'>Message cannot be empty!</div>
+        )}
+        {!numberEmptyError && !notAnInteger && !messageEmptyError && (
+          <div className='errors-null'>.</div>
+        )}
         <div className='search_contact app'>
- 
+          <CssTextField
+            error={numberEmptyError || notAnInteger}
+            label='Mobile Number'
+            placeholder='Mobile Number'
+            name='mobileNumber'
+            value={mobileNumber}
+            onChange={onChange}
+            size='small'
+            style={{
+              margin: "1em 0em",
+            }}
+            inputProps={{
+              style: textFieldStyle,
+            }}
+            InputLabelProps={{
+              style: textFieldInputLabelStyle,
+            }}
+            required
+          />
         </div>
-        <div className='message'></div>
-        <Button
-          onClick={(e) => {
-            // window.open(url)
-          }}
-        >
-          Send
-        </Button>
+        <div className='message app' style={{ marginTop: "1.5em" }}>
+          <CssTextField
+            multiline
+            maxRows={4}
+            label='Message'
+            placeholder='Hi! Payment reminder....'
+            size='small'
+            InputLabelProps={{
+              style: textFieldInputLabelStyle,
+            }}
+            inputProps={{
+              style: {
+                width: "230px",
+                height: "90px",
+              },
+              maxLength: CHARACTER_LIMIT,
+            }}
+            FormHelperTextProps={{
+              style: {
+                margin: 0,
+                padding: "0 0 0 5px",
+                fontSize: 10,
+              },
+            }}
+            name='message'
+            value={message}
+            onChange={onChange}
+            required
+            error={message.length > CHARACTER_LIMIT - 1 || messageEmptyError}
+            helperText={
+              !(message.length > CHARACTER_LIMIT - 1)
+                ? `${message.length}/${CHARACTER_LIMIT}`
+                : "Max length exceeded"
+            }
+          />
+        </div>
+        <div style={{ marginTop: "1.5em" }}>
+          <Button onClick={onSubmit} variant='outlined' color='success' size='small' >
+            Send
+          </Button>
+        </div>
       </div>
     </div>
   );
