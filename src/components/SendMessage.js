f624f551fcaab3402fc094b9915d4c417978a296
aunshx <aunsh.sb@gmail.com>
2022-03-19 09:03:56 +0530

    ready for medium

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -49,7 +49,6 @@ const textFieldStyle = {
 const SendMessages = () => {
   const CHARACTER_LIMIT = 100;
 
-  const URL = "https://web.whatsapp.com/send";
 
     const [numberEmptyError, setNumberEmptyError] = useState(false);
     const [messageEmptyError, setMessageEmptyError] = useState(false);
@@ -79,12 +78,7 @@ const SendMessages = () => {
         setMessageEmptyError(true)
         setTimeout(() => setMessageEmptyError(false), 3000)
     } else {
-        let number = mobileNumber.replace(/[^\w\s]/gi, "").replace(/ /g, "");
-
-        let url = `${URL}?phone=${number}`;
-
-       url += `&text=${encodeURI(message)}&app_absent=0`;
-      window.open(url)
+        //TODO: Whatsapp message send code here
     }
   }
 
