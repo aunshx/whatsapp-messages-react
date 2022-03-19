2d8a8aa5f8fc6a6d14e026657d8550d59a23a61f
aunshx <aunsh.sb@gmail.com>
2022-03-19 09:45:02 +0530

    creator links to the files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import SendMessages from "./components/SendMessage";
+import Footer from "./components/Footer";
+import CreatorLink from "./components/CreatorLink";
+
 import "./App.css";
 
 function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }} className='flex_middle'>
+        <CreatorLink />
         <SendMessages />
+        <Footer />
     </div>
   );
 }
